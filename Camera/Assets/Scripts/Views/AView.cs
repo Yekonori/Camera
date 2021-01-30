02eafddb98d06959a55f2eae56feb97084880f61
Alexis <alexis.sainsard@gmail.com>
2021-01-30 14:34:54 +0100

    Free follow view by default on global volume + organization floders

---
+++ Camera/Assets/Scripts/Views/AView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AView : MonoBehaviour
+{
+    [Min(0f)] public float weight;
+
+    public abstract CameraConfiguration GetConfiguration();
+
+    public void SetActive(bool isActive)
+    {
+        //SetActive(isActive);
+        if (isActive)
+        {
+            CameraController.Instance.AddView(this);
+        }
+        else
+        {
+            CameraController.Instance.RemoveView(this);
+        }
+    }
+
+    public virtual void OnDrawGizmos()
+    {
+        CameraConfiguration test = GetConfiguration();
+        if (test != null)
+        {
+            test.DrawGizmos(Color.white);
+        }
+    }
+}
